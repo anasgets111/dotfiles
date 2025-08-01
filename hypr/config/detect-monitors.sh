42506f4c18fdb8f1c1bec5caac3ea5d5a5ce0a2a
Anas Khalifa <anasgets111@gmail.com>
2025-08-01 04:47:01 +0300

    Add monitor configuration files and detection script

---
+++ hypr/config/detect-monitors.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+HOSTNAME="$(uname -n)"
+CONNECTED="$(hyprctl monitors | grep 'Monitor' | awk '{print $2}')"
+
+case "$HOSTNAME" in
+  "Wolverine")
+    if echo "$CONNECTED" | grep -q "DP-1"; then
+      SRC="$HOME/.config/hypr/config/desktop.conf"
+      DST="$HOME/.config/hypr/config/monitors.conf"
+      ln -sf "$SRC" "$DST"
+    else
+      SRC="$HOME/.config/hypr/config/laptop.conf"
+      DST="$HOME/.config/hypr/config/monitors.conf"
+      ln -sf "$SRC" "$DST"
+    fi
+    ;;
+  "Mentalist")
+    if echo "$CONNECTED" | grep -q "HDMI-A-1"; then
+      SRC="$HOME/.config/hypr/config/docked.conf"
+      DST="$HOME/.config/hypr/config/monitors.conf"
+      ln -sf "$SRC" "$DST"
+    else
+      SRC="$HOME/.config/hypr/config/laptop.conf"
+      DST="$HOME/.config/hypr/config/monitors.conf"
+      ln -sf "$SRC" "$DST"
+    fi
+    ;;
+esac
+
+hyprctl reload
