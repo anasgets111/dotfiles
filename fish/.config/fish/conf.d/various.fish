bdc46a6b1ff6788fa15b93a44bce734882b1c339
Anas Khalifa <anasgets111@gmail.com>
2025-11-29 18:55:14 +0200

    refactor: Change aliases to abbreviations in config.fish for improved readability fix: Add ignore_submodules option in starship.toml git_status configuration

--- fish/.config/fish/conf.d/various.fish
+++ fish/.config/fish/conf.d/various.fish
@@ -1,14 +1,12 @@
-
 ## User key bindings
 function fish_user_key_bindings
     # Bind Ctrl+Delete to delete the next word
 
-bind -k sdc  'commandline -f kill-line'
-bind \e\[3\;5~ 'commandline -f kill-word'
-bind \b 'commandline -f backward-kill-word'
+    bind -k sdc 'commandline -f kill-line'
+    bind \e\[3\;5~ 'commandline -f kill-word'
+    bind \b 'commandline -f backward-kill-word'
 
 end
-
 
 function sail
     if test -f sail
@@ -18,13 +16,10 @@ function sail
     end
 end
 
-
 ## Backup Function
 function backup --argument filename
     cp $filename $filename.bak
 end
-
-
 
 ## Copy Function
 function copy
@@ -37,8 +32,6 @@ function copy
         cp $argv
     end
 end
-
-
 
 function sendText
     # Use the first argument as the filename; default to file.txt if not provided
@@ -54,27 +47,4 @@ function sendText
 
     # Upload data from standard input, with secret=1 and expires set as calculated
     curl -F "file=@-;filename=$filename" -F "secret=1" -F "expires=$expiry" https://0x0.st
-end
-
-
-
-
-if not test "$fish_key_bindings" = "fish_vi_key_bindings"
-    set -e FISH__BIND_MODE 2>/dev/null
-end
-
-function rerender_on_bind_mode_change --on-variable fish_bind_mode
-    if test "$fish_key_bindings" = "fish_vi_key_bindings"
-        if test "$fish_bind_mode" != paste -a "$fish_bind_mode" != "$FISH__BIND_MODE"
-            set -gx FISH__BIND_MODE $fish_bind_mode
-            omp_repaint_prompt
-        end
-    else
-        # Clear if somehow in non-vi mode but variable remains
-        set -e FISH__BIND_MODE 2>/dev/null
-    end
-end
-
-function fish_default_mode_prompt --description "Display vi prompt mode"
-    # This function is masked and does nothing
 end
