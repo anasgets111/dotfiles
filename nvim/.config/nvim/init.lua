392e52574fcc4c70f49c916b8034e163d4ed37c4
Anas Khalifa <anasgets111@gmail.com>
2025-12-16 01:18:01 +0200

    nvim update

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -476,20 +476,34 @@ require('lazy').setup({
       end
     end,
   },
-  { -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+{ -- Highlight, edit, and navigate code
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local ok, configs = pcall(require, "nvim-treesitter.configs")
+    if not ok then
+      return
+    end
+
+    configs.setup({
+      ensure_installed = {
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "php",
+        "blade",
+        "rust",
+      },
       auto_install = true,
       highlight = {
         enable = true,
-        additional_vim_regex_highlighting = { 'ruby' },
       },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-  },
+      indent = {
+        enable = true,
+      },
+    })
+  end,
+},
 }, {
   ui = {
     icons = vim.g.have_nerd_font and {} or {
