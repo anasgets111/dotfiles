4d020258e4ff953c30d6f87dfb2ce46d15480511
Anas Khalifa <anasgets111@gmail.com>
2026-01-17 00:37:04 +0200

    Refactor fish configuration: streamline environment setup, update history management, and enhance abbreviations

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,62 +1,54 @@
-# Shared global color variables
-set -g GREEN (set_color green)
-set -g BLUE (set_color blue)
-set -g CYAN (set_color cyan)
-set -g RED (set_color red)
-set -g YELLOW (set_color yellow)
-set -g RESET (set_color normal)
+# 1. Environment
+fish_add_path -a /mnt/Work/0Coding/Exercism
 
-# set -g fish_key_bindings fish_vi_key_bindings
+# 2. Context-Aware History (Ordered by priority: Generic -> Specific)
+set -gx fish_history fish
+set -q TERMINAL_EMULATOR; and set -gx fish_history phpstorm
+set -q VSCODE_INJECTION; and set -gx fish_history vscode
+set -q CURSOR_TRACE_ID; and set -gx fish_history cursor
+string match -q Zed "$TERM_PROGRAM"; and set -gx fish_history zed
 
-# Fish Greeting
+# 3. Tool Initialization
+type -q zoxide; and zoxide init fish --cmd cd | source
+type -q starship; and starship init fish | source
+
+# 4. Smart Greeting (Runs only in pure terminals)
 function fish_greeting
-    if not set -q ZED_TERM; and not set -q VSCODE_INJECTION; and not set -q TERMINAL_EMULATOR
-        fastfetchy
+    if test "$fish_history" = fish
+        type -q fastfetch; and fastfetch -c $HOME/.config/fastfetchTheme.jsonc
     end
 end
 
-# choose history file per terminal type
-if set -q ZED_TERM
-    set -gx fish_history zed
-else if set -q VSCODE_INJECTION
-    set -gx fish_history vscode
-else if set -q TERMINAL_EMULATOR
-    set -gx fish_history phpstorm
-else
-    set -gx fish_history fish
-end
-
-# Aliases
-# Alias to drop caches
-abbr drop-cache 'sudo paccache -rk3; and paru -Sc --aur --noconfirm'
-abbr orphans 'pacman -Qdtq | sudo pacman -Rns -'
-abbr mirror 'sudo rate-mirrors --protocol https --allow-root --save /etc/pacman.d/mirrorlist --disable-comments-in-file arch'
-abbr mirror-aur 'sudo rate-mirrors --disable-comments-in-file --protocol=https --allow-root --save /etc/pacman.d/chaotic-mirrorlist chaotic-aur'
-abbr exercism "/mnt/Work/0Coding/exercism-3.5.4-linux-x86_64/exercism"
-abbr ola "docker exec -it ollama ollama"
-abbr fixpacman "sudo rm /var/lib/pacman/db.lck"
-abbr art 'php artisan'
+# 5. Abbreviations
+# --- Arch Maintenance ---
 abbr pacin 'sudo pacman -S'
 abbr pacinn 'sudo pacman -S --needed'
-abbr pacre 'sudo pacman -R'
-abbr pacrem 'sudo pacman -Rsn'
+abbr pacrem 'sudo pacman -Rns'
+abbr orphans 'pacman -Qdtq | sudo pacman -Rns -'
+abbr mirrors 'sudo rate-mirrors --protocol https --allow-root --save /etc/pacman.d/mirrorlist --disable-comments-in-file arch'
+abbr mirror-aur 'sudo rate-mirrors --disable-comments-in-file --protocol=https --allow-root --save /etc/pacman.d/chaotic-mirrorlist chaotic-aur'
+
+abbr drop-cache 'sudo paccache -rk3; and sudo pacman -Sc --noconfirm'
+abbr fixpacman 'sudo rm /var/lib/pacman/db.lck'
+
+# --- Dev Stack ---
+abbr art 'php artisan'
 abbr gl 'git pull'
 abbr gp 'git push'
-abbr hw 'hwinfo --short' # Hardware Info
-abbr big "expac -H M '%m\t%n' | sort -h | nl" # Sort installed packages according to size in MB
+abbr ola 'docker exec -it ollama ollama'
+abbr dots 'cd $DOTFILES'
+
+# --- Navigation ---
 abbr .. 'cd ..'
 abbr ... 'cd ../..'
 abbr .... 'cd ../../..'
 abbr ..... 'cd ../../../..'
-abbr ...... 'cd ../../../../..'
-alias fastfetchy="fastfetch -c $HOME/.config/fastfetchTheme.jsonc"
-abbr errors "journalctl -p 3 -xb"
-abbr tb 'nc termbin.com 9999'
-abbr rip "expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
-alias cd z
-abbr dots 'cd $DOTFILES; and stow '
-abbr ssh 'kitty +kitten ssh'
 
-# Initialization for Tools
-zoxide init fish | source
-starship init fish | source
+# --- Utilities ---
+alias fastfetchy 'fastfetch -c $HOME/.config/fastfetchTheme.jsonc'
+abbr tb 'nc termbin.com 9999'
+abbr errors 'journalctl -p 3 -xb'
+abbr hw 'hwinfo --short'
+abbr ssh 'kitty +kitten ssh'
+abbr big "expac -H M '%m\t%n' | sort -h | nl"
+abbr rip "expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
