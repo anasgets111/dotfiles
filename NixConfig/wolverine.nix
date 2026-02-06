c955d5f37bfa4608a390d7e3686f7fd42fdc53da
Anas Khalifa <anasgets111@gmail.com>
2026-02-06 22:12:10 +0200

    feat(greetd): add default session configuration for tuigreet

--- NixConfig/wolverine.nix
+++ NixConfig/wolverine.nix
@@ -44,6 +44,10 @@
     enable = true;
     withUWSM = true;
   };
+  services.greetd.settings.default_session = {
+    command = "${lib.getExe pkgs.tuigreet} --time --asterisks --remember --remember-user-session --cmd '${lib.getExe pkgs.uwsm} start hyprland.desktop' --theme 'border=magenta;prompt=cyan;input=green;time=yellow;action=blue;button=cyan;container=black;text=white'";
+    user = "greeter";
+  };
 
   # Gaming & Performance
   programs.steam.enable = true;
