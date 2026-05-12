6dd3770cd725afa717f2c80dd58c9e4148f71b96
Anas Khalifa <anasgets111@gmail.com>
2026-05-12 05:30:48 +0300

    hypr: optimize configuration with iterators and reduced boilerplate

--- hypr/.config/hypr/config/input.lua
+++ hypr/.config/hypr/config/input.lua
@@ -1,15 +1,15 @@
 hl.config({
-	input = {
-		kb_layout = "us,eg",
-		kb_options = "grp:alt_shift_toggle,lv3:ralt_alt",
-		numlock_by_default = true,
-		touchpad = {
-			natural_scroll = true,
-		},
-	},
-	gestures = {
-		workspace_swipe_distance = 700,
-	},
+    input = {
+        kb_layout = "us,eg",
+        kb_options = "grp:alt_shift_toggle,lv3:ralt_alt",
+        numlock_by_default = true,
+        touchpad = {
+            natural_scroll = true,
+        },
+    },
+    gestures = {
+        workspace_swipe_distance = 700,
+    },
 })
 
 hl.gesture({ fingers = 3, direction = "horizontal", action = "workspace" })
