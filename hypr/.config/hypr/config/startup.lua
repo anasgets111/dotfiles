2249231f1954b7c802b6beffd37a3a3922194cf6
Anas Khalifa <anasgets111@gmail.com>
2026-08-18 16:33:38 +0300

    trying something for startup ordering

--- hypr/.config/hypr/config/startup.lua
+++ hypr/.config/hypr/config/startup.lua
@@ -2,7 +2,6 @@ local startup_commands = {
     "hyprctl setcursor Bibata-Modern-Ice 24",
     "uwsm app -- $CARGOBIN/hyprland-per-window-layout",
     "sh -c 'while true; do cat /dev/input/by-id/usb-3537_Controller_00006F64096B22E0-event-joystick >/dev/null 2>&1; sleep 1; done'",
-    "uwsm app -- quickshell",
     "uwsm app -- wl-clip-persist --clipboard regular",
     "uwsm app -- kdeconnectd",
     "uwsm app -- chromium",
@@ -13,6 +12,7 @@ local startup_commands = {
     "uwsm app -- vesktop",
     "uwsm app -- slack",
     "uwsm app -- yerdd",
+    "uwsm app -- quickshell",
 }
 
 hl.on("hyprland.start", function()
